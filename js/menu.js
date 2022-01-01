8717333e2986566a54b70e3d8916e20d53f528a8
ilaew <96941463+ilaew@users.noreply.github.com>
2022-01-01 21:55:38 +0800

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+// 为了方便在所有页面中快速更改目录，用了js，修改请先转成html
+document.writeln("<li><a href=\'../\'><span class=\'iconfontsmall icon-home\'></span> Home</a></li>");
+document.writeln("<li><a href=\'../about/\'><span class=\'iconfontsmall icon-about\'></span> About</a></li>");
+document.writeln("<li><a href=\'../articles/\'><span class=\'iconfontsmall icon-article\'></span> Articles</a></li>");
+document.writeln("<li><a href=\'../works/\'><span class=\'iconfontsmall icon-app\'></span> Works</a></li>");
+document.writeln("<br> <b>Theme by <a href=\'https://ravelloh.github.io\'>RavelloH</a>:<a href=\'https://github.com/RavelloH/RTheme\' class=\'linkline\'>RTheme</a><b>");
